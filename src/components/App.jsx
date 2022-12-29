61c5609d5d41db57991a5d7fd4fe39fb66859675
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-12-29 20:24:19 +0100

    update task
    delete usless elements prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import { Component } from 'react';
 import PhoneBook from './PhoneBook/PhoneBook';
 class App extends Component {
-  
   render() {
     return (
       <>
-       <PhoneBook title='PhoneBook App' 
-       formSubmissionHandler={(data)=>{console.log(data);}}/>
+        <PhoneBook
+          title="PhoneBook App"
+          formSubmissionHandler={data => {
+            console.log(data);
+          }}
+        />
       </>
     );
   }
 }
-
 
 export default App;
--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -11,47 +11,67 @@ class PhoneBook extends Component {
             {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
             {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
             {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
-        name:``,
-        number:""      }
+        name:"",
+        number:"",
+        search:""     }
 
-    //   handleChange = event =>{
-    //     this.setState({name:event.target.value,
-    //                     number:event.target.value })
-    //     console.log(this.state);
-    //   }
-      
       handleInputChange =(event)=>{
-        // const value =event.target.value;
-        // const name = event.target.name;
-
         const {value, name} =event.target
-        console.log(`tetst `, {[name]:value});
         this.setState({
             [name]:value
         })
       }
 
-
       handleSubmit = (event) =>{
         event.preventDefault();
         console.log(this.state)
-        const object = {name:this.state.name, number:this.state.number}
+        const object = {id:nanoid(5), name:this.state.name, number:this.state.number}
         console.log(object);
         this.setState({
             name:'',
             number:'',
+            
         })
+
         this.state.contacts.push(object)
     }
 
     render(){
-        const {name, number} =this.state
+        const {name, number, search} =this.state
+        
         return(
             <div className={css.app}>
             <div>
                 {this.props.title}
             </div>
+
             <form className={css.form} onSubmit={this.handleSubmit}>
+                <label className={css.label}>Search
+                    <input
+                    className={css.input} 
+                    type='search' 
+                    name="search" 
+                    value={search} 
+                    onChange={this.handleInputChange}></input>
+                </label>
+                <ul className={css.listMain}>
+                    {   
+                        
+
+                        this.state.contacts.filter(contact=>{
+                            const searchType=this.state.search.toLowerCase();
+                            const contactType=contact.name.toLowerCase();
+                          return  contactType.includes(searchType)
+                            
+                        }).map((contact, index)=>{
+                            if(this.state.search){
+                            return(
+                                <li key={index}>{contact.name}:  {contact.number}</li>
+                            )
+                        }
+                    })
+                    }
+                </ul>            
                 <label className={css.label}>Name
                 <input className={css.input}
   type="text"
@@ -60,17 +80,18 @@ class PhoneBook extends Component {
   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
   value={name}
   onChange={this.handleInputChange}
+
   placeholder='Add contact'
   required
 />  
                 </label>
-                <label className={css.label}>
+                <label className={css.label}> Number
                 <input className={css.input}
   type="tel"
   name="number"
   value={number}
   onChange={this.handleInputChange}
-  placeholder='Number'
+  placeholder='Add Number'
   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
   required
@@ -83,7 +104,7 @@ class PhoneBook extends Component {
                     
                     this.state.contacts.map((contact, index)=>{
                 return (
-                    <li id={nanoid(5)} key={index}>{contact.name} :  {contact.number}</li>
+                    <li key={index}>{contact.name} :  {contact.number}</li>
                 )
                     })
                 }
